77392ab0e7ae85392366e6adfe426258a73b760d
maryam mrg <maryamelouadaa@gmail.com>
2025-07-01 09:59:14 +0100

    creating the Subskill repository

---
+++ src/main/java/com/example/enaaskill/Repository/SubSkillRepository.java
@@ -0,0 +1,8 @@
+package com.example.enaaskill.Repository;
+
+import com.example.enaaskill.Model.SubSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubSkillRepository extends JpaRepository<SubSkill, Long> {
+    
+}
